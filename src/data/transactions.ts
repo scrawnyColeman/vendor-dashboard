b2dc59dca7005f695bb65130d83631ff5d63d5b5
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-25 00:25:29 +0100

    dummy transaction data

---
+++ src/data/transactions.ts
@@ -0,0 +1,104 @@
+export const transactionsData: Transaction[] = [
+  {
+    transactionId: 1,
+    date: new Date(),
+    value: {
+      amount: 10.5,
+      currency: {
+        sym: '£',
+        name: 'GBP',
+      },
+    },
+    ref: 'vendor9f041j2k',
+    selectedReward: 'Beer on us',
+    customer: {
+      id: 1,
+      email: 'johnsmith@email.com',
+    },
+  },
+  {
+    transactionId: 2,
+    date: new Date(),
+    value: {
+      amount: 23.6,
+      currency: {
+        sym: '£',
+        name: 'GBP',
+      },
+    },
+    ref: 'vendor32i3j2i4',
+    selectedReward: '1 tree planted',
+    customer: {
+      id: 2,
+      email: 'joebloggs@email.com',
+    },
+  },
+  {
+    transactionId: 3,
+    date: new Date(),
+    value: {
+      amount: 12.35,
+      currency: {
+        sym: '£',
+        name: 'GBP',
+      },
+    },
+    ref: 'vendorl214jm04',
+    selectedReward: '1 tree planted',
+    customer: {
+      id: 1,
+      email: 'johnsmith@email.com',
+    },
+  },
+  {
+    transactionId: 4,
+    date: new Date(),
+    value: {
+      amount: 28.4,
+      currency: {
+        sym: '£',
+        name: 'GBP',
+      },
+    },
+    ref: 'vendork20s85jd',
+    selectedReward: '1 tree planted',
+    customer: {
+      id: 2,
+      email: 'joebloggs@email.com',
+    },
+  },
+  {
+    transactionId: 5,
+    date: new Date(),
+    value: {
+      amount: 34.4,
+      currency: {
+        sym: '£',
+        name: 'GBP',
+      },
+    },
+    ref: 'vendorjg9wk3mf',
+    selectedReward: '1 tree planted',
+    customer: {
+      id: 1,
+      email: 'johnsmith@email.com',
+    },
+  },
+  {
+    transactionId: 6,
+    date: new Date(),
+    value: {
+      amount: 9.5,
+      currency: {
+        sym: '£',
+        name: 'GBP',
+      },
+    },
+    ref: 'vendork20s85jd',
+    selectedReward: '1 tree planted',
+    customer: {
+      id: 3,
+      email: 'andrewryan@email.com',
+    },
+  },
+]
