7bafd0c6fba17608e64400906dddee7d619298f7
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-25 17:22:49 +0100

    add overflow for main content and api keys expand

--- src/components/Dashboard/Welcome/index.tsx
+++ src/components/Dashboard/Welcome/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../scss/style.scss'
-import { FaArrowRight } from 'react-icons/fa'
+import { FaArrowDown, FaArrowRight } from 'react-icons/fa'
 
 const Welcome: React.FC = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const toggleOpen = (): void => {
+    setIsOpen(!isOpen)
+  }
   return (
     <div className="dashboard-page-welcome">
       <div>
         <h2>Welcome aboard, Thomas</h2>
       </div>
       <div className="dashboard-page-api-keys">
-        <FaArrowRight /> Find your API keys or start taking payments in person
-        now
+        <div
+          role="button"
+          tabIndex={0}
+          onKeyDown={toggleOpen}
+          onClick={toggleOpen}
+          className="dashboard-page-api-keys-header"
+        >
+          {isOpen ? <FaArrowDown /> : <FaArrowRight />}
+          <p>Find your API keys or start taking payments in person now</p>
+        </div>
+        {isOpen && (
+          <div className="dashboard-page-api-keys-body">Some information</div>
+        )}
       </div>
     </div>
   )
