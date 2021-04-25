ce76034189b718cd1eabcf5b19e0b5c87fcf2a3b
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-25 16:17:06 +0100

    PaymentsPage

--- src/components/Payments/Page/index.tsx
+++ src/components/Payments/Page/index.tsx
@@ -1,16 +1,7 @@
 import React from 'react'
-import '../scss/style.scss'
-import UsersGrid from '../UsersGrid'
 
 const PaymentsPage: React.FC = (): JSX.Element => {
-  return (
-    <div className="payments-page-container">
-      <div className="payments-page-search-bar">Search</div>
-      <div className="payments-page-welcome">Welcome</div>
-      <div className="payments-page-cards">Cards</div>
-      <UsersGrid />
-    </div>
-  )
+  return <div>Payments Page</div>
 }
 
 export default PaymentsPage
