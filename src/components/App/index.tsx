385755a1dd13bdc28da0a4d77f2129b96c9d363b
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-25 18:25:52 +0100

    Attempting fix with HashRouter

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 import SideBar from '../Navigation/SideBar'
 import Routing from '../Routing'
 import './scss/style.scss'
 
 const App: React.FC = (): JSX.Element => {
   return (
-    <Router>
+    <>
       <SideBar />
       <div className="main-container">
         <Routing />
       </div>
-    </Router>
+    </>
   )
 }
 
